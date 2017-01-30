8d3a4770457a16f68331aed9eb896278e436eac8
kamleshpaliwal-tudip <kamlesh.paliwal@tudip.nl>
2017-01-30 13:58:30 +0530

    Feature #39295 -JSON assignment uploaded Feature #39276 -jQuery assignment uploaded

---
+++ secondweek/likedislike/js/count.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function(){
+                        obj={likes: 0, dislikes: 0};
+                        lcount=obj.likes;
+                        dcount=obj.dislikes;
+                        total=0;
+
+
+                        $("#like").click(function(){
+                                lcount=lcount+1;
+                                $("#ldisplay").text(lcount);
+                                obj.likes=lcount;
+                                if(typeof(Storage) !== "undefined") {
+                                    if (localStorage.likecount) {
+                                         localStorage.likecount = Number(localStorage.likecount)+1;
+                                     }
+                                     else {
+                                             localStorage.likecount = 1;
+                                           }
+
+                                     $("#likes").text(localStorage.likecount);
+                                     
+                                 }
+                        });
+
+                        $("#dis").click(function(){
+                                dcount++;
+                                $("#ddisplay").text(dcount);
+                                obj.dislikes=dcount;
+
+                                if(typeof(Storage) !== "undefined") {
+                                    if (localStorage.dislikecount) {
+                                         localStorage.dislikecount = Number(localStorage.dislikecount)+1;
+                                     }
+                                     else {
+                                             localStorage.dislikecount = 1;
+                                           }
+                                    $("#dislikes").text(localStorage.dislikecount);
+                                 
+                                 }
+                        });  
+                                  if(typeof(Storage) !== "undefined") {
+                                     localStorage.totalcount=Number(localStorage.likecount)+Number(localStorage.dislikecount);
+                                     $("total").text("hi");
+                                     }
+                                        $("total").text("hi");
+
+                   
+                        
+                        
+                         });
